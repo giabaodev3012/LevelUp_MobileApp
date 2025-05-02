2fa8c28394b1132980354a83af90378952957bd6
Gia Bao <nguyenluonggiabao301203@gmail.com>
2025-05-02 17:01:21 +0700

    Add ThemeProvider with light/dark theme and AsyncStorage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { ThemeProvider } from '@/context/theme.context'
+import { Stack } from 'expo-router'
 
 export default function _layout() {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <ThemeProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index"/>
+      </Stack>
+    </ThemeProvider>
   )
 }
